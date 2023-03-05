0fc866ca08ec9fb16222501ebda3b0614871559a
YUNQI <56862159+YunqiZHAO@users.noreply.github.com>
2023-03-05 01:44:19 +0000

    Update login-style.js

--- CS5041-P2/styles/login-style.js
+++ CS5041-P2/styles/login-style.js
@@ -1,2 +1,31 @@
 
 // styling for the login page
+
+import { StyleSheet } from "react-native-web";
+
+const logInStyles = StyleSheet.create({
+    view: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    usernameinput: {
+        margin: 20,
+        height: 40,
+        width: "50%",
+        fontSize: 20,
+        placeholderTextColor: "grey",
+    },
+
+    button: {
+        backgroundColor:"#DDDDDD",
+        height: 50,
+        width: 100,
+        borderRadius: 15,
+        margin: 10,
+        justifyContent: "center"
+        
+    }
+});
+
+export default logInStyles;
